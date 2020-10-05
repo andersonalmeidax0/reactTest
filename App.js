582f458531d1eff86f807eddce31688fd1377de3
andersonalmeidax0 <36134545+andersonalmeidax0@users.noreply.github.com>
2020-10-05 12:28:08 -0300

    Update App.js

--- App.js
+++ App.js
@@ -5,43 +5,40 @@ export  class NativeSample extends Component {
  constructor(props) {
      super(props)
      this.state = {
-      firstVar: ' ',
       count: 0,
       text:props.defaultText,
+      textColor:'blue',
      }
   }
   handleClick = () =>  {
-      this.setState((state) => {
-          return {count: this.state.count + 1};
-      });
+    this.setState((state) => {
+        return {count: this.state.count + 1};
+    });
   }
   handleChangeText  = (t) =>   {
     this.setState({text:t});
   }
   handleChangeTextState  = (t) =>   {
-    this.setState({text:"######"});
+    this.setState({text:"New value"});
   }
   handleChangeTextProps  = (t) =>   {
-    this.setState({text:t});
+    //this.setState({text:t});
+    if(this.state.textColor=='blue')
+      this.setState({textColor:'red'});
+    else  
+      this.setState({textColor:'blue'});
   }
 
  render() {
   return (
    <View style={{margin:50}}>
-    <Text style={{padding: 10, fontSize: 16}}>TestApp v1.5</Text>
+    <Text style={{padding: 10, fontSize: 16}}>TestApp v1.6</Text>
     <Button title="SetCounterState" onPress={this.handleClick}  color = "red" />
     <Text style={{ height: 20,  borderWidth: 0, fontSize: 16 }}>CountValue:</Text>
     <Text style={{ height: 20,  borderWidth: 0, fontSize: 16 }}>{this.state.count}</Text>
-    <TextInput style={{height: 32, borderWidth: 2}} placeholder="Type your text!" 
-    onChangeText={(firstVar) => this.setState({firstVar})}   />
-
     <TextInput style={{height: 32,borderColor: 'gray', borderWidth: 2}} placeholder="Initial TEXT"
-          onChangeText={(t) => this.setState({text:t})} value={this.state.text} 
-      />
-
-      
-    <Text style={{padding: 10, fontSize: 16}}>  {this.state.firstVar}  </Text>
-    <Text style={{padding: 10, fontSize: 16}}>  {this.state.text}  </Text>
+          onChangeText={(t) => this.setState({text:t})} value={this.state.text}   />
+    <Text style={{padding: 10, fontSize: 16, color:this.state.textColor }}>  {this.state.text}  </Text>
     <Button title="Change Text State" onPress={this.handleChangeTextState}  color = "red" />
     <Button title="Change Text Props" onPress={this.handleChangeTextProps}  color = "red" />
    </View>
