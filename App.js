643cf264a906b0ec2fb5380473b10963403eb5c2
andersonalmeidax0 <36134545+andersonalmeidax0@users.noreply.github.com>
2020-10-05 14:21:57 -0300

    Update App.js

--- App.js
+++ App.js
@@ -1,16 +1,20 @@
+//TESTE de StateUpdate
+
 import React, { Component } from 'react';
-import {  AppRegistry, StyleSheet, Text, TextInput, View, Button} from 'react-native';
+import {  AppRegistry, StyleSheet, Text, TextInput, View, Button, Switch} from 'react-native';
+
 
 //https://habiletechnologies.com/blog/understanding-the-basic-components-of-react-native/
 //funcionou em andoir e IOS
 
-export  class NativeSample extends Component {
+export  class StateComponent extends Component {
  constructor(props) {
      super(props)
      this.state = {
       count: 0,
       text:props.defaultText,
       textColor:'blue',
+      toggle:false,
      }
   }
   handleClick = () =>  {
@@ -43,14 +47,40 @@ export  class NativeSample extends Component {
     <Text style={{padding: 10, fontSize: 16, color:this.state.textColor }}>  {this.state.text}  </Text>
     <Button title="Change Text State" onPress={this.handleChangeTextState}  color = "red" />
     <Button title="Change Text Props" onPress={this.handleChangeTextProps}  color = "red" />
+    <Switch />
+
+       <Switch
+          trackColor={{false: 'gray', true: 'teal'}}
+          thumbColor="blue"
+          ios_backgroundColor="gray"
+          onValueChange={(value) => this.setState({toggle: value})}
+          value={this.state.toggle}
+        />
+    <Text style={{ height: 20,  borderWidth: 0, fontSize: 16 }}>toggle:{this.state.toggle==true?"true":"false"}</Text>
+
    </View>
    );
  }
 }
 
 export default class App1 extends Component {
+  handleChangeTextState  = (t) =>   {
+    this.setState({text:"New value"});
+  }
+  handleChangeTextProps  = (t) =>   {
+    if(this.state.textColor=='blue')
+      this.setState({textColor:'red'});
+    else  
+      this.setState({textColor:'blue'});
+  }
   render() {
-    return (<NativeSample  defaultText="INITIAL111" />)
+    return (
+      <View>
+        <StateComponent  defaultText="INITIAL111" />
+        <Button title="Change Child Props" onPress={this.handleChangeTextProps}  color = "blue" />
+        <Button title="Change Child State" onPress={this.handleChangeTextProps}  color = "blue" />
+      </View>
+      )
   }
 }
 
