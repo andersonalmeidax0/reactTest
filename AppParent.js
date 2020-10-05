d934bdd6c21465fa6f6b219075722060ff58b7cf
andersonalmeidax0 <36134545+andersonalmeidax0@users.noreply.github.com>
2020-10-05 14:34:41 -0300

    Update AppParent.js

--- AppParent.js
+++ AppParent.js
@@ -1,6 +1,10 @@
-//import * as React from 'react';
-//import { Text, View, StyleSheet } from 'react-native';
-//import {  AppRegistry,  TextInput, Component, Button, Drawer} from 'react-native';
+//Test de CHILD UPDATE FROM PARENT
+//causas de erro no "super"
+//0-import errado de componentes ( ex import componente de "react-native" ao inves de reat)
+//1-Forgotten export The first culprit is usually that you have forgotten to export the component that you are trying to use and hence React finds that the Component you are trying to extend from is undefined 
+//2-Using a default export incorrectly If a component is exported with the default keyword then it must be imported as a default import and not as a named import.
+//3-circular dependencies
+
 
 import React, { Component } from 'react';
 import {  AppRegistry, StyleSheet, Text, TextInput, View, Button, Drawer, Switch} from 'react-native';
